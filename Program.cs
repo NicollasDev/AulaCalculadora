a9e1495aa9901f1538944534708023c7c2fcbc78
NicollasDileo06 <nicgato2013@gmail.com>
2020-06-03 20:43:32 -0300

    Calculadora

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula09_Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            CalculadoraCientifica calc = new CalculadoraCientifica();
+
+            Console.WriteLine("Digite uma operação no formato de ex: 12 x 3");
+            calc.ControlarMetodos( Console.ReadLine() );
+            Console.WriteLine("Resultado: "+calc.resultado);
+
+            Console.WriteLine("Digite números separados por ponto e vírgula para calcularmos a média");
+            calc.CalcularMedia( Console.ReadLine() );
+            Console.WriteLine("Média: "+calc.resultado);
+
+        }
+    }
+}
